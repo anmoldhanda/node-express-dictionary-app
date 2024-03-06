d6c733a5f7078fb54a0261e1e8fa953f43aa21a1
anmol dhanda <rk352714@gmail.com>
2024-03-06 12:15:12 +0530

    final commit

---
+++ readme.md
@@ -0,0 +1,35 @@
+# Dictionary App
+
+This is a simple dictionary web application built using Express.js, RapidAPI, and HTML. It allows users to search for the definitions of words.
+
+## Features
+
+- User can search for the definition of a word.
+- Definitions are fetched from the RapidAPI Dictionary API.
+- Simple and intuitive user interface.
+
+## Technologies Used
+
+- Node.js
+- Express.js
+- RapidAPI
+- HTML
+- CSS
+
+## Installation
+
+1. Clone the repository:
+2. Install dependencies:
+3. Set up RapidAPI account and get API key.
+4. Replace `<YOUR_RAPIDAPI_KEY>` with your actual RapidAPI key in `app.js`.
+
+## Usage
+
+1. Start the server:
+2. Open your web browser and go to `http://localhost:3000`.
+3. Enter the word you want to search for in the input field and click "Submit".
+4. The definition of the word will be displayed on the page.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require("express");
+const { request } = require("http");
+const axios = require("axios").default;
+const { chownSync } = require("fs");
+const path = require("path");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/searchword", (req, res) => {
+  //   res.send("dictionary api");
+  let apiword = req.query.word;
+  let networkrequestdetails = {
+    // api credentials
+    method: "GET",
+    url: "https://dictionary-by-api-ninjas.p.rapidapi.com/v1/dictionary",
+    params: { word: apiword },
+    headers: {
+      "X-RapidAPI-Key": "your api key",
+      "X-RapidAPI-Host": "your api host",
+    },
+  };
+
+  // demo data for testing purpose in future
+  // let response = {};
+  // response.data = {
+  //   definition:
+  //     "See Brite, v. i.\n" +
+  //     "\n" +
+  //     "1. Radiating or reflecting light; shedding or having much light; shining; luminous; not dark. The sun was bright o'erhead. Longfellow. The earth was dark, but the heavens were bright. Drake. The public places were as bright as at noonday. Macaulay. 2. Transmitting light; clear; transparent. From the brightest wines He 'd turn abhorrent. Thomson. 3. Having qualities that render conspicuous or attractive, or that affect the mind as light does the eye; resplendent with charms; as, bright beauty. Bright as an angel new-dropped from the sky. Parnell. 4. Having a clear, quick intellect; intelligent. 5. Sparkling with wit; lively; vivacious; shedding cheerfulness and joy around; cheerful; cheery. Be bright and jovial among your guests. Shak. 6. Illustrious; glorious. In the brightest annals of a female reign. Cotton. 7. Manifest to the mind, as light is to the eyes; clear; evident; plain. That he may with more ease, with brighter evidence, and with surer success, draw the bearner on. I. Watts. 8. Of brilliant color; of lively hue or appearance. Here the bright crocus and blue violet grew. Pope. Note: Bright is used in composition in the sense of brilliant, clear, sunny, etc.; as, bright-eyed, bright-haired, bright-hued. Syn. -- Shining; splending; luminous; lustrous; brilliant; resplendent; effulgent; refulgent; radiant; sparkling; glittering; lucid; beamy; clear; transparent; illustrious; witty; clear; vivacious; sunny.\n" +
+  //     "\n" +
+  //     "Splendor; brightness. [Poetic] Dark with excessive bright thy skirts appear. Milton.\n" +
+  //     "\n" +
+  //     "Brightly. Chaucer. I say it is the moon that shines so bright. Shak.\n" +
+  //     "\n" +
+  //     "To be or become overripe, as wheat, barley, or hops. [Prov. Eng.]",
+  //   word: apiword,
+  //   valid: true,
+  // };
+  // console.log(response.data.word);
+  // console.log(response.data.definition);
+  // response.json(apidata.data);
+
+  axios
+    .request(networkrequestdetails)
+    .then((response) => {
+      // console.log(networkrequestdetails.params);
+      console.log(response.data.word);
+      console.log(response.data.definition);
+      res.json(response.data);
+      // sending the api response back to the client side
+    })
+    .catch((error) => {
+      console.log(error);
+      // if any kind of internal server error occured
+      res.status(500).send("error occured");
+    });
+});
+
+app.listen(port, () => {
+  console.log(`example app listening on port http://localhost:${port}`);
+});
